2c31041f89b3cbbf1eac709dcae73c36138862da
David Palma <davidpalmalugo@gmail.com>
2019-04-19 23:31:19 -0400

    correcion en urls de proyecto

--- jdmclasic/urls.py
+++ jdmclasic/urls.py
@@ -26,7 +26,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auto/', include('apps.jdm.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    #path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('accounts/password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
