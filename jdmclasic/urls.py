6fc75b7feb7dec440889da2fbb14792a378e496e
David Palma <davidpalmalugo@gmail.com>
2019-04-19 21:45:40 -0400

    Primeras configuraciones de modulo de autenticacion

--- jdmclasic/urls.py
+++ jdmclasic/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+#from django.contrib.auth.views import login, logout_then_login, password_reset, password_reset_done, password_reset_confirm, password_reset_complete
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auto/', include('apps.jdm.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
